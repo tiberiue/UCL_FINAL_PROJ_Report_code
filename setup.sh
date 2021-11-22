9a47f966eaf888c9e223be790ee06d7f905f930a
Jad Zahreddine <jad.zahreddine@l2it.in2p3.fr>
2021-11-22 20:33:06 +0100

    Add different codes

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#source /pbs/software/centos-7-x86_64/anaconda/3.7/etc/profile.d/conda.sh
+source /sps/atlas/j/jzahredd/LJPTagger/GNN/miniconda3/bin/activate
+conda activate rootenv
+
