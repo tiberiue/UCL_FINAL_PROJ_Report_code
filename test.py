134e57b5c3abb6aee2f9c12d8a8f032a3973e834
Jad Mathieu Sardain <jad.sardain@cern.ch>
2022-06-24 05:07:40 +0200

    change test.pt

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import uproot
 
-
+print("Hello world")
+#file = ROOT.TFile.Open("root://eosuser.cern.ch//eos/user/j/jzahredd/JetTagging/final/Train_Test_split_lxplus/user.mykhando.27420846._000001.tree.root_train.root")
 #file = "root://eosuser.cern.ch//eos/user/j/jzahredd/JetTagging/final/Train_Test_split_lxplus/user.mykhando.27420846._000001.tree.root_train.root"
-file = "/eos/user/j/jzahredd/JetTagging/final/Train_Test_split_lxplus/user.mykhando.27420846._000001.tree.root_train.root"
-dsids = []
-with uproot.open(file) as infile:
-    tree = infile["FlatSubstructureJetTree"]
-    dsids.append(tree["DSID"].array())
+#file = "/eos/user/j/jzahredd/JetTagging/final/Train_Test_split_lxplus/user.mykhando.27420846._000001.tree.root_train.root"
+#dsids = []
+#with uproot.open(file) as infile:
+#    tree = infile["FlatSubstructureJetTree"]
+#    dsids.append(tree["DSID"].array())
 
-print (dsids)
+#print (dsids)
