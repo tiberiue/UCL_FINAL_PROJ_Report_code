4e019921de19e074089a7d86b5f0373dfd87726e
Jad Mathieu Sardain <jad.sardain@cern.ch>
2022-06-24 03:09:36 +0200

    simpler test

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
 import uproot
 
-file = "root://eosuser.cern.ch//eos/user/j/jzahredd/JetTagging/final/Train_Test_split_lxplus/user.mykhando.27420846._000001.tree.root_train.root"
-#file = "/eos/user/j/jzahredd/JetTagging/final/Train_Test_split_lxplus/user.mykhando.27420846._000001.tree.root_train.root"
-dsids = []
-with uproot.open(file) as infile:
-    tree = infile["FlatSubstructureJetTree"]
-    dsids.append(tree["DSID"].array())
-
-print (dsids)
+print("Hello world")
