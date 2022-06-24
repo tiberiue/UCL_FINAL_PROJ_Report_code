09f1b3f9393cafc9abcb18edb7570ccc4bd52136
Jad Mathieu Sardain <jad.sardain@cern.ch>
2022-06-24 04:01:25 +0200

    add root file to test.py

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 import uproot
 
-print("Hello world")
+
+#file = "root://eosuser.cern.ch//eos/user/j/jzahredd/JetTagging/final/Train_Test_split_lxplus/user.mykhando.27420846._000001.tree.root_train.root"
+file = "/eos/user/j/jzahredd/JetTagging/final/Train_Test_split_lxplus/user.mykhando.27420846._000001.tree.root_train.root"
+dsids = []
+with uproot.open(file) as infile:
+    tree = infile["FlatSubstructureJetTree"]
+    dsids.append(tree["DSID"].array())
+
+print (dsids)
